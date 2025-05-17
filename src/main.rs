0008c2c0547d79e0b8632e1fc4b2176a0f4ac59e
Cameron VanHouzen <cameron@vanhouzen.me>
2025-05-17 18:02:39 -0400

    Switch to Maud Templates (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,83 @@
-use axum::{response::IntoResponse, routing::get};
+use axum::{
+    response::{IntoResponse, Redirect},
+    routing::get,
+};
 use dotenvy::dotenv;
-use rinja::Template;
+use fastrace::collector::{Config, ConsoleReporter};
+use fastrace_axum::FastraceLayer;
+use log::error;
+use maud::{DOCTYPE, Markup, html};
 use tower_http::services::ServeDir;
 use tower_livereload::LiveReloadLayer;
+
+mod cameron;
 
 // Entrypoint for axum application
 #[tokio::main]
 async fn main() {
-    dotenv().expect(".env file not found");
+    if let Err(error) = dotenv() {
+        error!("failed to load env file, error: {error}")
+    }
 
-    tracing_subscriber::fmt::init();
+    // Setup logging out to the console
+    logforth::stdout().apply();
+
+    fastrace::set_reporter(ConsoleReporter, Config::default());
 
     let app = axum::Router::new()
         .nest_service("/assets", ServeDir::new("assets"))
-        .fallback(get(index))
-        .layer(LiveReloadLayer::new());
+        .nest("/cameron", cameron::router())
+        .route("/", get(index))
+        .fallback(async || Redirect::to("/"))
+        .layer(FastraceLayer);
+
+    #[cfg(debug_assertions)]
+    let app = app.layer(LiveReloadLayer::new());
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
+    fastrace::flush();
 }
 
-#[tracing::instrument]
+#[fastrace::trace]
+fn layout(title: &str, content: Markup) -> Markup {
+    html! {
+        (DOCTYPE)
+        html {
+            head {
+                title { (title) }
+                meta charset="utf-8";
+                meta name="viewport" content="width=device-width, initial-scale=1";
+                link rel="stylesheet" href="/assets/pico.min.css";
+                link rel="stylesheet" href="/assets/style.css";
+            }
+            body {(content)}
+        }
+    }
+}
+
+#[fastrace::trace]
 async fn index() -> impl IntoResponse {
-    IndexTemplate {}
+    let content = html! {
+        main {
+            h1 { "Welcome to the VanHouzen Family!" }
+            p { "This site houses the personal pages for the VanHouzen family and friends" }
+            h2 { "VanHouzens" }
+            ul {
+                li { a href="/cameron" { "Cameron VanHouzen" } }
+                li { a href="/courtney" { "Courtney VanHouzen" } }
+                li { a href="/wade" { "Wade VanHouzen" } }
+                li { a href="/janet" { "Janet VanHouzen" } }
+            }
+            h2 { "Friends" }
+            p { "Checkout the websites of our friends!" }
+            ul {
+                li { a href="/schrider" { "Schriders" } }
+                li { a href="/pobudas" { "Pobudas" } }
+            }
+        }
+    };
+    layout("Home", content)
 }
-
-#[derive(Template)]
-#[template(path = "index.html")]
-struct IndexTemplate {}
