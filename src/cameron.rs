2c4586e44a2224a32af21de941e5c595cae9e762
Cameron VanHouzen <camvh@live.com>
2025-07-02 10:36:45 -0400

    added stub for cameron page

--- src/cameron.rs
+++ src/cameron.rs
@@ -1,12 +1,11 @@
 use axum::{response::IntoResponse, routing::get};
-use maud::html;
-
-use crate::layout;
+use maud::{DOCTYPE, Markup, html};
 
 pub fn router() -> axum::Router {
     axum::Router::new().route("/", get(cameron))
 }
 
+#[fastrace::trace]
 async fn cameron() -> impl IntoResponse {
     let content = html!(
         main {
@@ -16,4 +15,35 @@ async fn cameron() -> impl IntoResponse {
         }
     );
     layout("Cameron", content)
+}
+
+#[fastrace::trace]
+fn layout(title: &str, content: maud::Markup) -> Markup {
+    html!(
+        (DOCTYPE)
+        html {
+            head {
+                title { (title) }
+                meta charset="utf-8";
+                meta name="viewport" content="width=device-width, initial-scale=1";
+                link rel="stylesheet" href="/assets/pico.min.css";
+                link rel="stylesheet" href="/assets/style.css";
+            }
+            body {
+                header {
+                    nav {
+                        ul {
+                            li { a href="/" { "Home" } }
+                            li { a href="/about" { "About" } }
+                            li { a href="/projects" { "Projects" } }
+                        }
+                    }
+                }
+            }
+            main {(content)}
+            footer {
+                p { "Copyright © 2025 Cameron VanHouzen" }
+            }
+        }
+    )
 }
